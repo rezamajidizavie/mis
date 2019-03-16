d5a2701ff60f4131b3afd706b1eb01d6aa0dc429
Reza Majidi <rezamajidizavie@gmail.com>
2019-03-17 00:23:46 +0330

    first commit

---
+++ config/keys.js
@@ -0,0 +1,3 @@
+module.exports = {
+  mongoURI:'mongodb://rezamajidizavie:eldreza6620548@ds247944.mlab.com:47944/mis'
+}
---
+++ routes/api/users.js
@@ -0,0 +1,7 @@
+const express = require("express");
+const router = express.Router();
+
+//@access : public
+router.get("/test", (req, res) => res.json({ msg: "users works properly" }));
+
+module.exports = router;
