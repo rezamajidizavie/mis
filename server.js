6937095e301a8a3cf22c1df6fcc60f9d25f07467
Reza Majidi <rezamajidizavie@gmail.com>
2019-03-29 18:15:13 +0430

    add cors

--- server.js
+++ server.js
@@ -3,6 +3,7 @@ const mongoose = require("mongoose");
 const bodyParser = require("body-parser");
 const passport = require("passport");
 const path = require("path");
+const cors = require("cors");
 
 const users = require("./routes/api/users");
 const profile = require("./routes/api/profile");
@@ -13,6 +14,7 @@ const app = express();
 // body-parser middleware
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
+app.use(cors());
 
 // DB config
 const db = require("./config/keys").mongoURI;
