4359f69f32213c375ab772ef1ab33e1ab4cfc1d7
Reza Majidi <rezamajidizavie@gmail.com>
2019-03-29 17:30:54 +0430

    Change footer

--- client/src/components/Footer.js
+++ client/src/components/Footer.js
@@ -1,14 +1,12 @@
-import React, { Component } from 'react';
+import React, { Component } from "react";
 
 class Footer extends Component {
   render() {
     return (
       <div>
-      <footer className="bg-dark text-white mt-5 p-4 text-center">
-        تمامی حقوق محفوظ است {new Date().getFullYear()}
-        <br/>
-        Reza Majidi
-      </footer>
+        <footer className="bg-dark text-white mt-5 p-4 text-center">
+          تمامی حقوق محفوظ است {new Date().getFullYear()} - Reza Majidi
+        </footer>
       </div>
     );
   }
