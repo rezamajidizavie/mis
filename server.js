943f9d64c496bb838b78578d86aafb8a2c9f7b86
Reza Majidi <rezamajidizavie@gmail.com>
2019-03-30 22:49:42 +0430

    Add favicon

--- server.js
+++ server.js
@@ -41,10 +41,10 @@ app.use("/api/users", users);
 app.use("/api/profile", profile);
 app.use("/api/posts", posts);
 
-app.use(express.static(path.join(__dirname, "client", "build")));
-app.get("*", (req, res) => {
-  res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
-});
+// app.use(express.static(path.join(__dirname, "client", "build")));
+// app.get("*", (req, res) => {
+//   res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
+// });
 
 const port = process.env.PORT || 5000;
 
