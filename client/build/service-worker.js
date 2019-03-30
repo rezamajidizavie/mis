25cf85d941a16907b0a17a6ea450677bf280bd62
Reza Majidi <rezamajidizavie@gmail.com>
2019-03-30 15:27:38 +0430

    m

--- client/build/service-worker.js
+++ client/build/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.beb364b5018ea8195fb0f1f94bccf675.js"
+  "/precache-manifest.a92abf1146d378e9deb31c68f4199d04.js"
 );
 
 workbox.clientsClaim();
